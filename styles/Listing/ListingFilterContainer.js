94695751f37130322dbd666b32e96e9d7f00ccd5
Nicolas Melo <nicolasmelo12@gmail.com>
2020-03-18 01:03:52 -0300

    created datepicker

--- styles/Listing/ListingFilterContainer.js
+++ styles/Listing/ListingFilterContainer.js
@@ -4,7 +4,7 @@ export default styled.div`
     position: absolute;
     width: 80vw;
     max-width: 600px;
-    z-index: 1;
+    z-index: 10;
     background-color: #444;
     border-radius: 5px; 
     padding: 10px
