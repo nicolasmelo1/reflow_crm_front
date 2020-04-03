732ee5a9e4be9e7935297ec064f8d505b8851a7a
Nicolas Melo <nicolasmelo12@gmail.com>
2020-04-03 17:59:01 -0300

    remove some files with issues

---
+++ components/Navbar/NavbarDropdown.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { NavDropdown } from 'react-bootstrap'
+import { NavbarLinkIcon, NavbarLinkLabel} from 'styles/Navbar'
+
+
+const NavbarDropdown = (props) => {
+    return (
+        <NavDropdown 
+        title={
+            <span>
+                <NavbarLinkIcon icon={props.icon} />
+                <NavbarLinkLabel>{props.label}</NavbarLinkLabel>
+            </span>
+        }>
+            {props.items.map((item, index)=> {
+                return (
+                    <NavDropdown.Item as="button" key={index} href={item.href} onClick={e => { (item.onClick) ? item.onClick(e): null }}>
+                        {item.label}
+                    </NavDropdown.Item>
+                )
+            })}
+        </NavDropdown>
+    )
+}
+
+export default NavbarDropdown
