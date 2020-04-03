143130b13f8d272c46620714b0ac84ce03169afe
Nicolas Melo <nicolasmelo12@gmail.com>
2020-04-03 17:56:29 -0300

    remove some files with issues

--- components/Utils/index.js
+++
@@ -1,7 +0,0 @@
-import Select from './Select'
-import DateRangePicker from './DateRangePicker'
-import DateTimePicker from './DateTimePicker'
-
-export { Select }
-export { DateRangePicker }
-export { DateTimePicker }
