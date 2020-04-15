c43d638fc039ef3d1a67fccb57da6f03818aa686
Nicolas Melo <nicolasmelo12@gmail.com>
2020-04-15 16:46:05 -0300

    commit

--- merge.js
+++ merge.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 
 const args = process.argv.slice(2);
 
+// keeps package.json in sync with the root package.json.
 args.forEach(arg=> {
     let localPackageJson = require(`./${arg}/package.json`)
     const sharedPackageJson = require('./package.json')
