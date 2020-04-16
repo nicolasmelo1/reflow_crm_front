0f065a634555d1295c2bddbb9bc0cc24051cf37b
Nicolas Melo <nicolasmelo12@gmail.com>
2020-04-16 18:00:19 -0300

    try to fix build issues in web

--- merge.js
+++ merge.js
@@ -3,11 +3,11 @@ const fs = require('fs')
 const args = process.argv.slice(2);
 
 // keeps package.json in sync with the root package.json.
-args.forEach(arg=> {
+/*args.forEach(arg=> {
     let localPackageJson = require(`./${arg}/package.json`)
     const sharedPackageJson = require('./package.json')
 
     localPackageJson.dependencies = sharedPackageJson.dependencies
 
     fs.writeFileSync(`./${arg}/package.json`, JSON.stringify(localPackageJson, null, 2))
-})
+})*/
