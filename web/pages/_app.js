1faa7d807a71cdfb41be835692a6540e27f73c27
Nicolas Melo <nicolasmelo12@gmail.com>
2020-04-17 17:18:56 -0300

    fix issue with viewport height in android

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -8,6 +8,9 @@ import { initStore } from '@shared/redux/store';
 
 
 class MyApp extends App {
+
+    // You might want to check this https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+    // we need this for the formulary height that was causing some bugs on chrome on android phones.
     appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
 
     componentDidMount() {
