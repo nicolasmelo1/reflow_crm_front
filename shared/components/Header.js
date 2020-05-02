e391a00ef7d5056bee4bc068e151781468acfef0
Nicolas Melo <nicolasmelo12@gmail.com>
2020-05-02 17:08:42 -0300

    added pwa funcionality

--- shared/components/Header.js
+++ shared/components/Header.js
@@ -8,10 +8,27 @@ import React from 'react'
  * title (String) - Defines the title of the page
  * */
 const Header = (props) => (
+
     <Head>
         <title>{ props.title }</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+        <meta charset="utf-8" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="white"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width, viewport-fit=cover"/>
+        <meta name="name" content="Reflow"/>
+        <meta name="description" content="Changing the way people work"/>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#0dbf7e" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/pwa/images/icons/icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="96x96" href="/pwa/images/icons/icon-96x96.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/pwa/images/icons/icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/pwa/images/icons/icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="192x192" href="/pwa/images/icons/icon-192x192.png"/>
+        <link rel="apple-touch-icon" sizes="384x384" href="/pwa/images/icons/icon-384x384.png"/>
+        <link rel="apple-touch-icon" sizes="512x512" href="/pwa/images/icons/icon-512x512.png"/>
+        <link rel="icon" type="image/png" sizes="72x72" href="/pwa/images/icons/icon-72x72.png"/>
+        <link rel="manifest" href="/pwa/manifest.json" />
+        
         <script type="text/javascript" src="/dragdroptouch/js/dragdroptouch.js"></script>
         <link rel="stylesheet" href="/bulma/css/bulma.min.css" />
         <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'></link>
