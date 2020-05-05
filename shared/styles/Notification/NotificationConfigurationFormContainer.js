ba07bbe8d0d5953635e7f70b2777d9dbed7bb68d
Nicolas Melo <nicolasmelo12@gmail.com>
2020-05-05 20:24:11 -0300

    i think i fixed the most common bugs

--- shared/styles/Notification/NotificationConfigurationFormContainer.js
+++ shared/styles/Notification/NotificationConfigurationFormContainer.js
@@ -5,7 +5,7 @@ import { View } from 'react-native'
 export default process.env['APP'] === 'web' ? 
 styled.div`
     margin: 0 10px;
-    background-color: #f2f2f2;
+    background-color: #fff;
     padding: 5px 0;
 `
 :
