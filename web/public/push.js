799659bc9c4eca0984e787ef605ea8c50d9844fa
Nicolas Melo <nicolasmelo12@gmail.com>
2020-05-20 22:30:51 -0300

    forgot to add delay on task

--- web/public/push.js
+++ web/public/push.js
@@ -7,7 +7,8 @@ function receivePushNotification(event) {
     const { image, tag, url, title, text, actions } = event.data.json();
     
     const imageUrl = image ? image : self.location.origin+'/pwa/images/icons/icon-512x512.png'
-
+    console.log('imageUrl')
+    console.log(imageUrl)
     const options = {
         data: url,
         body: text,
@@ -27,11 +28,15 @@ function openPushNotification(event) {
 
     // if we recieve a full url we open the window the backend wants us to open
     if (event.notification.data.contains('https://') || event.notification.data.contains('http://')) {
+        console.log('Entrou aqui')
+        console.log(event.notification.data)
         event.waitUntil(clients.openWindow(event.notification.data))
     } else {
         // handles the open of the url for different types
         // The backend doesn't need to be aware of any url of the front end, this needs to be handled here.
         if (event.notification.data === 'notification') {
+            console.log('entrou no notification')
+            console.log(self.location.origin + '/notifications')
             event.waitUntil(clients.openWindow(self.location.origin + '/notifications'));
         }
     }
