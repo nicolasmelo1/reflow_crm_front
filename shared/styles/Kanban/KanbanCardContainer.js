5dfe9d5d1c8c9fec30d3df0d87a0f325408b01af
Nicolas Leal <nicolasmelo12@gmail.com>
2020-12-27 02:12:39 -0300

    small enhancements in the UI and design overall of the system

--- shared/styles/Kanban/KanbanCardContainer.js
+++ shared/styles/Kanban/KanbanCardContainer.js
@@ -5,6 +5,7 @@ import { View } from 'react-native'
 export default process.env['APP'] === 'web' ?
 styled.div`
     box-shadow: 0 3px 6px #17242D;
+    border: 1px solid #bfbfbf;
     border-radius: 5px;
     padding: 10px;
     width: 250px;
