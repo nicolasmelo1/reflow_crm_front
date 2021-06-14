7bb0c3beb8a9a9ca7266179d86e76cb43ae588ce
Nicolas Melo <nicolasmelo@MacBook-Pro-de-Nicolas.local>
2021-06-13 23:00:53 -0300

    fix small issue when creating formula fields

--- shared/components/Formulary/FormularySectionEdit.js
+++ shared/components/Formulary/FormularySectionEdit.js
@@ -106,6 +106,7 @@ const FormularySectionEdit = (props) => {
             form: props.section.id,
             number_configuration_mask: '9',
             formula_configuration: null,
+            field_formula_variables: [],
             is_long_text_rich_text: false,
             label_name: '',
             placeholder: '',
