2f7c7041b823f5da4bf9121852461e1a665ef7ab
Nicolas Melo <nicolasmelo@MacBook-Pro-de-Nicolas.local>
2021-07-21 19:15:42 -0300

    added hotjar to front end

--- web/components/Header.js
+++ web/components/Header.js
@@ -47,6 +47,7 @@ const Header = (props) => {
             <style>
                 {fonts.map(font => font)}
             </style>
+            <script type="text/javascript" src="/hotjar/index.js"/>
             <script type="text/javascript" src="/stonly/index.js"/>
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144919276-1"/>
             <script type="text/javascript" src="/google/analytics/index.js"/>
