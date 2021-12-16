f13288f09795b8cb15fc4b0a6f7c1aefc3e122bf
Nicolas Melo <nicolasmelo@MacBook-Pro-de-Nicolas.local>
2021-12-16 02:37:11 -0300

    better manage the tracking algorithm

--- shared/utils/track.js
+++ shared/utils/track.js
@@ -1,11 +1,11 @@
 export const trackOpenedOnboarding = () => {
-    if (process.env['APP'] === 'web' && process.env.NODE_ENV==='production') {
+    if (process.env['APP'] === 'web' && process.env.NODE_ENV==='production' && fbq !== undefined) {
         fbq('track', 'Lead')
     }
 }
 
 export const trackFinishedOnboardingProcess = () => {
-    if (process.env['APP'] === 'web' && window.lintrk && process.env.NODE_ENV==='production') {
+    if (process.env['APP'] === 'web' && window.lintrk && process.env.NODE_ENV==='production' && fbq !== undefined) {
         window.lintrk('track', { 
             conversion_id: 6503801 
         })
